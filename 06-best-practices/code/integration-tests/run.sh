ae1b4744f665837df3a522e6fb5dbfe1f566bd0f
peter716 <adedejioluwadara7@gmail.com>
2024-07-04 21:39:55 +0000

    created terraform files

--- 06-best-practices/code/integration-tests/run.sh
+++ 06-best-practices/code/integration-tests/run.sh
@@ -2,13 +2,23 @@
 
 cd "$(dirname "$0")"
 
-LOCAL_TAG=`date +"%Y-%m-%d-%H-%M"`
-export LOCAL_IMAGE_NAME="stream-model-duration:${LOCAL_TAG}"
-export PREDICTIONS_STREAM_NAME="ride_predictions"
+# LOCAL_TAG=`date +"%Y-%m-%d-%H-%M"`
+# export LOCAL_IMAGE_NAME="stream-model-duration:${LOCAL_TAG}"
+# export PREDICTIONS_STREAM_NAME="ride_predictions"
 
-export AWS_DEFAULT_REGION="eu-north-1"
+# docker build -t ${LOCAL_IMAGE_NAME} ..
 
-docker build -t ${LOCAL_IMAGE_NAME} ..
+if [ "${LOCAL_IMAGE_NAME}" == "" ]; then
+    LOCAL_TAG=`date +"%Y-%m-%d-%H-%M"`
+    export LOCAL_IMAGE_NAME="stream-model-duration:${LOCAL_TAG}"
+    export AWS_DEFAULT_REGION="eu-north-1"
+    echo "LOCAL_IMAGE_NAME is not set, building a new image with tag ${LOCAL_IMAGE_NAME}"
+    docker build -t ${LOCAL_IMAGE_NAME} ..
+else
+    echo "no need to build image ${LOCAL_IMAGE_NAME}"
+fi
+
+
 
 docker-compose up -d
 
